89c800575c5f3fe6abda7a06e3878b5df439d520
Steef Min <steefmin@gmail.com>
2015-12-15 10:47:50 +0100

    removed loop over Serial.write() on receiver
    as each character is send one at a time, it might be enough to just write the single character of the buffer

--- chat.ino
+++ chat.ino
@@ -30,9 +30,9 @@ void loop() {
         // Read the received data
         //vw_wait_rx_max(10);                             // can we commment this out?
         if (vw_get_message(buf, &buflen)){
-                int i;
-                for (i = 0; i < buflen; i++){
-                    Serial.write(buf[i]);
-                }     
+                //int i;
+                //for (i = 0; i < buflen; i++){
+                    Serial.write(buf[1]);
+                //}     
     }
 }
